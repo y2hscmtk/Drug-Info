882c35b455502c0a6f8f4325114d7f79c2299dbb
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-01-15 20:22:54 +0900

    ADD : DrugTableViewCell

---
+++ Drug-Info/Drug-Info/cell/DrugTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DrugTableViewCell.swift
+//  Drug-Info
+//
+//  Created by Choi76 on 2024/01/15.
+//
+
+import UIKit
+
+class DrugTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
