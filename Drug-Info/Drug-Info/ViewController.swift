6cca5b26cd027eff9724d82d33ee7f9176a71665
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-01-15 17:33:50 +0900

    project init

---
+++ Drug-Info/Drug-Info/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Drug-Info
+//
+//  Created by Choi76 on 2024/01/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
