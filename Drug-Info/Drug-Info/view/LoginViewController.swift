8ad04e9c1e2892848ec2a9897f9c34d1238509c9
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-05-15 21:19:16 +0900

    💄 회원가입 뷰 연결

--- Drug-Info/Drug-Info/view/LoginViewController.swift
+++ Drug-Info/Drug-Info/view/LoginViewController.swift
@@ -10,21 +10,36 @@ import UIKit
 // 로그인
 class LoginViewController: UIViewController {
 
+    
+    @IBOutlet weak var userIdTexField: UITextField!
+    
+    @IBOutlet weak var userPasswordTextField: UITextField!
+    
+    @IBOutlet weak var signInLabel: UILabel! // 회원가입 버튼 용으로 사용하기 위한 라벨
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        signInLabel.isUserInteractionEnabled = true
+        
+        let signInLabelTapGesture = UITapGestureRecognizer(target: self, action: #selector(signInLabelDidTapped))
+        signInLabel.addGestureRecognizer(signInLabelTapGesture) // 이벤트 등록
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func loginBtnDidTapped(_ sender: UIButton) {
+        
+        
     }
-    */
-
+    
+    @objc private func signInLabelDidTapped(sender : UITapGestureRecognizer){
+        // 1. 스토리보드 찾기
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        // 2. 이동할 뷰 찾기 => 스토리보드의 identifier를 통해
+        let signUpViewController = storyboard
+            .instantiateViewController(identifier: "SignUpViewController") as SignUpViewController
+        signUpViewController.modalPresentationStyle = .fullScreen //전체 화면으로 변경
+        // 3. 화면 이동
+        navigationController?.pushViewController(signUpViewController, animated: true)
+    }
+    
 }
---
+++ Drug-Info/Drug-Info/view/MainTabBarViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MainTabBarViewController.swift
+//  Drug-Info
+//
+//  Created by Choi76 on 5/15/24.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // 네비게이션 바 숨기기
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        // 다른 뷰로 이동할 때 네비게이션 바를 다시 보이게 설정
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+
+}
