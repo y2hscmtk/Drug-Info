6c08ce31e8a566244cad5e2c5ccfb36113bd0fa8
Kang Choi / 최 강 <109474668+y2hscmtk@users.noreply.github.com>
2024-05-15 22:24:32 +0900

    ✨ 회원가입 기능 보완
    오류 메시지 출력, 회원가입 완료 후 로그인 뷰로 되돌아가기

--- Drug-Info/Drug-Info/view/SignUpViewController.swift
+++ Drug-Info/Drug-Info/view/SignUpViewController.swift
@@ -13,45 +13,73 @@ class SignUpViewController: UIViewController {
 
     
     @IBOutlet weak var emailTextField: UITextField!
-    
+    @IBOutlet weak var emailValidLabel: UILabel!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var passwordValidLabel: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+            
+        // 초기에는 오류 문구 보이지 않도록 설정
+        emailValidLabel.isHidden = true
+        passwordValidLabel.isHidden = true
     }
     
     @IBAction func signUpButtonTapped(_ sender: Any) {
-        
-        guard let email = emailTextField.text, !email.isEmpty,
-              let password = passwordTextField.text, !password.isEmpty else {
-            print("Missing email or password")
+        // 이메일이 비어있는지 확인
+        guard let email = emailTextField.text, !email.isEmpty else{
+            emailValidLabel.isHidden = false
+            emailValidLabel.text = "이메일을 작성해주세요."
             return
         }
         
+        // 비밀번호가 비어있는지 확인
+        guard let password = passwordTextField.text, !password.isEmpty else{
+            passwordValidLabel.isHidden = false
+            passwordValidLabel.text = "비밀번호를 작성해주세요."
+            return
+        }
+        
+        // 이메일 유효성 검사
         guard isValidEmail(email) else {
-            print("Invalid email format")
+            emailValidLabel.isHidden = false
+            emailValidLabel.text = "이메일 형식이 올바르지 않습니다."
             return
         }
 
         Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
-            if let error = error {
-                print("Failed to sign up: \(error.localizedDescription)")
+            if let error = error as NSError? {
+                if let errorCode = AuthErrorCode.Code(rawValue: error.code) {
+                    switch errorCode {
+                    case .emailAlreadyInUse:
+                        self.emailValidLabel.isHidden = false
+                        self.emailValidLabel.text = "이미 사용중인 이메일입니다."
+                    case .invalidEmail:
+                        self.emailValidLabel.isHidden = false
+                        self.emailValidLabel.text = "이메일 형식이 올바르지 않습니다."
+                    case .weakPassword:
+                        self.passwordValidLabel.isHidden = false
+                        self.passwordValidLabel.text = "비밀번호는 6글자 이상이어야 합니다."
+                    default:
+                        print("Error: \(error.localizedDescription)")
+                    }
+                }
                 return
             }
             // 회원가입 성공
             print("User signed up: \(authResult?.user.uid ?? "")")
-            // 로그인 화면으로 이동 또는 자동 로그인 처리
+            // FireStorage에 회원 저장 과정 작성 필요(uid 저장 필요)
+            self.navigationController?.popViewController(animated: true)
         }
     }
     
     
     // 유효성 검사
     private func isValidEmail(_ email: String) -> Bool {
-       let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-       let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
-       return emailPred.evaluate(with: email)
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: email)
     }
 
 }
